43be504bac0ef96c4466d868dcea212d27fb31b2
Agavni Zarukian <zar.aghavni@gmail.com>
2022-01-18 19:29:44 +0100

    Add comments

--- src/test/java/com/demo/inputforms/CheckboxTest.java
+++ src/test/java/com/demo/inputforms/CheckboxTest.java
@@ -22,38 +22,60 @@ public class CheckboxTest extends TestCase {
     }
 
     public void testSingleCheckBox() {
+        // Given
         driver.get("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
         driver.findElement(By.id("isAgeSelected")).click();
-        String text = driver.findElement(By.id("txtAge")).getText();
+
+        // When
+        final String text = driver.findElement(By.id("txtAge")).getText();
+
+        // Then
         assertEquals("Success - Check box is checked", text);
     }
 
     public void testMultipleCheckbox() {
+        // Given
         driver.get("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
         driver.findElement(By.id("check1")).click();
-        boolean isChecked = driver.findElement(By.className("cb1-element")).isSelected();
+
+        // When
+        final boolean isChecked = driver.findElement(By.className("cb1-element")).isSelected();
+        final String btnName = driver.findElement(By.id("check1")).getAttribute("value");
+
+        // Then
         assertTrue(isChecked);
-        String btnName = driver.findElement(By.id("check1")).getAttribute("value");
-        assertEquals("Uncheck All",btnName);
+        assertEquals("Uncheck All", btnName);
     }
+
     public void testMultipleCheckboxSimple() {
+        // Given
         driver.get("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
         driver.findElement(By.id("check1")).click();
-        String isChecked = driver.findElement(By.id("isChkd")).getAttribute("value");
+
+        // When
+        final String isChecked = driver.findElement(By.id("isChkd")).getAttribute("value");
+        final String btnName = driver.findElement(By.id("check1")).getAttribute("value");
+
+        // Then
         //Get attribute value as a String and converted to Boolean.
-        assertTrue( Boolean.parseBoolean(isChecked));
-        String btnName = driver.findElement(By.id("check1")).getAttribute("value");
-        assertEquals("Uncheck All",btnName);
+        assertTrue(Boolean.parseBoolean(isChecked));
+        assertEquals("Uncheck All", btnName);
     }
+
     public void testMultipleCheckboxUncheck() {
+        // Given
         driver.get("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
         //Check All
         driver.findElement(By.id("check1")).click();
         //Uncheck All
         driver.findElement(By.id("check1")).click();
+
+        // When
         boolean isChecked = driver.findElement(By.className("cb1-element")).isSelected();
-        assertFalse(isChecked);
         String btnName = driver.findElement(By.id("check1")).getAttribute("value");
-        assertEquals("Check All",btnName);
+
+        // Then
+        assertFalse(isChecked);
+        assertEquals("Check All", btnName);
     }
 }
